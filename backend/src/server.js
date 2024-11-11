33b222c27b80c8bb6bb749bffbce88e756e015da
Ivan Lovrić <ilovric@neos.hr>
2024-11-11 03:19:46 +0100

    Add starting backend express app.

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+const host = process.env.HOST;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Server running on: http://${host}:${port}/`);
+});
